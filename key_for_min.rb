0dd50b67d6a35ed6a4ade878de44bee1c81edc29
Mario Garza <mario.garza02@utrgv.edu>
2020-04-20 19:37:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,8 @@ def key_for_min_value(name_hash)
     return nil 
   end
   name_arr.collect do |key|
-    
+  end
+  new_hash=name_arr[0]
   name_hash.each do |key,value|
     if(value<smallestValue)
       smallestValue=value
