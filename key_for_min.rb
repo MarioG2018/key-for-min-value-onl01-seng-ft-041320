d4bc3781b6f877015712d440323378133aa8ac29
Mario Garza <mario.garza02@utrgv.edu>
2020-04-20 19:58:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,13 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_arr=[]
+  value_arr=[]
   if(name_hash.empty?)
     return nil 
   end
   name_hash.each do |key,value|
-    name_arr.push(value)
+    value_arr.push(value)
   end
+  
 end
 
 #def key_for_min_value(hash)
